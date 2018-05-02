b546a4a69b1defe7eab53f16fe9cc1112a4ca900
Ferenc Kálmán <ferenckalman1997@gmail.com>
2018-05-02 17:10:58 +0200

    Cleanup and add icon

--- server/app.js
+++ server/app.js
@@ -3,26 +3,52 @@ const database = require('child_process').fork('server/database.js');
 const CWD = 'file://' + process.cwd();
 let window;
 
-app.on('ready', createWindow);
-
 database.send('connect');
-app.on('window-all-closed', () =>{
+app.on('ready', createWindow);
+app.on('window-all-closed', handleClose);
+
+
+// Create the browser window.
+function createWindow(){
+    Menu.setApplicationMenu(null);
+    app.setName('TrainGod');
+
+    window = new BrowserWindow({
+        title: 'TrainGod',
+        backgroundColor: '#84290A',
+        show: false,
+        width: 1024,
+        height: 768,
+        minWidth: 320,
+        minHeight: 240,
+        webPreferences: {devTools: true},
+        icon: CWD + '/img/icon.png'
+    });
+
+    globalShortcut.register('F12', () => window.toggleDevTools());
+    window.loadURL(CWD + '/html/index.html');
+    window.once('ready-to-show', () =>{
+        // window.maximize();
+        window.show();
+        window.focus();
+    });
+
+    window.on('closed', () => window = null);
+}
+
+function handleClose(){
     let force = null;
-    console.log('PEACEFUL');
     database.send('close');
 
-    // FUCK THIS SHIT ~Feca at 5.32AM
     Promise.race([
         new Promise(res => database.on('exit', res)),
         new Promise(res =>{
             force = setTimeout(() =>{
-                console.log('HARSH');
                 database.kill('SIGTERM');
                 res();
             }, 1000);
         }).then(() => new Promise(res =>{
             force = setTimeout(() =>{
-                console.log('BRUTAL');
                 database.kill('SIGINT');
                 res();
             }, 1000);
@@ -35,33 +61,4 @@ app.on('window-all-closed', () =>{
     }).catch(() =>{
         console.log('Failed to quit automatically.');
     });
-});
-
-
-// Create the browser window.
-function createWindow(){
-    Menu.setApplicationMenu(null);
-    app.setName("TrainGod");
-
-    window = new BrowserWindow({
-        title: 'TrainGod',
-        backgroundColor: '#84290A',
-        show: false,
-        width: 1024,
-        height: 768,
-        minWidth: 320,
-        minHeight: 240,
-        webPreferences: {devTools: true},
-        icon: __dirname + '/train.png'
-    });
-
-    globalShortcut.register('F12', () => window.toggleDevTools());
-    window.loadURL(CWD + '/html/index.html');
-    window.once('ready-to-show', () =>{
-        // window.maximize();
-        window.show();
-        window.focus();
-    });
-
-    window.on('closed', () => window = null);
 }
